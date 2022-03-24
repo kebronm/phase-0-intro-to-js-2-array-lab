0dd87f5fc963d1a0ddb64eb83a5108f2d08b9b53
Kebron Mehari <kebronmehari400@gmail.com>
2022-03-24 15:44:21 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,75 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+
+function destructivelyAppendCat(name){
+  console.log(["Milo", "Otis", "Garfield", name])
+
+  cats.push("Ralph");
+  cats;
+
+}
+
+destructivelyAppendCat('Ralph');
+
+
+function destructivelyPrependCat(name){
+  console.log([name, "Milo", "Otis", "Garfield",])
+
+  
+cats.unshift("Bob");
+
+
+cats;
+
+}
+
+destructivelyAppendCat('Bob');
+
+
+function  destructivelyRemoveLastCat(){
+  console.log(["Milo", "Otis", "Garfield",])
+
+  
+  cats.pop();
+
+
+cats;
+
+}
+
+function  destructivelyRemoveFirstCat(){
+  console.log(["Milo", "Otis", "Garfield",])
+
+  
+  cats.shift();
+
+
+cats;
+
+}
+
+function appendCat(name){
+    var newArray = cats.slice();
+    newArray.push(name)
+    return newArray
+  }
+  appendCat('Broom');
+
+  function prependCat(name){
+    var newArray = cats.slice();
+    newArray.unshift(name)
+    return newArray
+  }
+  prependCat("Arnold");
+
+  function removeLastCat(){
+    var newArray = ["Milo", "Otis", "Garfield",]
+    newArray.splice(2);
+    return newArray
+  }
+  
+  function removeFirstCat(){
+    var newArray = ["Milo", "Otis", "Garfield",]
+    newArray.shift();
+    return newArray
+  }
